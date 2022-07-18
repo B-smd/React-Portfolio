5a034d74595849ded0b8804f0bb3f2725ac343ae
B-smd <djaja@iinet.net.au>
2022-07-18 15:39:57 +0800

    link and icon

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -16,8 +16,8 @@ const ContactMe = () => {
             <textarea className="form-control" id="comments" rows="3"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
-            <a href="https://webmail.iinet.net.au/djaja@iinet.net.au"><img src="https://img.icons8.com/fluent/48/000000/mail--v1.png" alt="Mail" className="icon" width={"56"}/></a>
-            <a><img src="https://img.icons8.com/fluent/48/000000/mobile--v1.png" alt="Mail" className="icon" width={"56"}/>+61 413 406 968</a>
+            <a href="mailto:djaja@iinet.net.au?subject=message"><img src="https://img.icons8.com/fluent/48/000000/mail--v1.png" alt="Mail" className="icon" width={"56"}/></a>
+            <a href="tel: +61 413 406 968"><img src="https://img.icons8.com/fluent/48/000000/mobile--v1.png" alt="Mobile" className="icon" width={"56"}/></a>
 
         </form>
     )
