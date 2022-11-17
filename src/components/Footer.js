48dcead99eb8e3052c0b18e656dbe0cb2c154659
B-smd <djaja@iinet.net.au>
2022-11-17 10:58:11 +0800

    update Screenshot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <nav className="container-fluid navbar navbar-expand-sm justify-content-center">
       <div className="row">
         <a href="https://github.com/B-smd"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon" width={"56"}/></a>
-        <a href="https://twitter.com/johndjaja"><img src="https://img.icons8.com/fluent/48/000000/twitter--v1.png" alt="Twitter" className="icon" width={"56"}/></a>
+        <a href="https://twitter.com/michadjaja"><img src="https://img.icons8.com/fluent/48/000000/twitter--v1.png" alt="Twitter" className="icon" width={"56"}/></a>
         <a href="https://www.linkedin.com/in/bambang-sugiarto-micha-djaja-53ba8b6b"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon" width= "56" /></a>
       </div>
     </nav>
